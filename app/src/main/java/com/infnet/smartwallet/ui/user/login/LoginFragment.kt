39c5088a081407cdbbd848a0c9ab3d5efd24a85f
Bruno Verdan Dantas Rodrigues <bruno.verdan@ed.company>
2021-03-22 18:27:13 -0300

    Bug fix: Consertado o bottom navigation na tela de login

--- app/src/main/java/com/infnet/smartwallet/ui/user/login/LoginFragment.kt
+++ app/src/main/java/com/infnet/smartwallet/ui/user/login/LoginFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.infnet.smartwallet.R
 import com.infnet.smartwallet.ui.ticket.list.ListTicketsViewModel
 import kotlinx.android.synthetic.main.cadastro_fragment.*
@@ -23,6 +24,10 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var view = inflater.inflate(R.layout.login_fragment, container, false)
+
+        val bottomNavigationView: BottomNavigationView = requireActivity().findViewById(R.id.bottomNavigationTickets)
+        bottomNavigationView.visibility = View.GONE
+
         viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
         viewModel.status.observe(viewLifecycleOwner, Observer {
             if(it){
