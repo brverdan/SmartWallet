59bda95382ad4da846cfd228efa5b56bbb11f801
Bruno Verdan Dantas Rodrigues <bruno.verdan@ed.company>
2021-03-23 09:12:42 -0300

    Criação do banner de ads

--- app/src/main/java/com/infnet/smartwallet/MainActivity.kt
+++ app/src/main/java/com/infnet/smartwallet/MainActivity.kt
@@ -3,9 +3,14 @@ package com.infnet.smartwallet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,7 +21,20 @@ class MainActivity : AppCompatActivity() {
 
         bottomNavigationTickets.setupWithNavController(findNavController(R.id.navHostFragment))
 
-       val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationTickets)
-       bottomNavigationView.visibility = View.GONE
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationTickets)
+        bottomNavigationView.visibility = View.GONE
+
+        MobileAds.initialize(this)
+
+        val mAdView = this.findViewById<AdView>(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+
+        mAdView.loadAd(adRequest)
+
+        mAdView.adListener = object : AdListener() {
+            override fun onAdClicked() {
+                Log.i("AdMob", "Anúncio clicado")
+            }
+        }
     }
 }
